3feeba0db21144b6d7413097d0cba9c9cfb06239
Michael Pak <pak.junho.michael@gmail.com>
2017-06-09 23:20:04 -0700

    react calculator test - configuration

---
+++ react-calculator/calculator.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Calculator from './calculator';
+
+describe("Calculator", () => {
+  let mountedCalculator;
+
+  beforeEach(() => {
+    mountedCalculator = mount(<Calculator />);
+  }
+
+
+
+
+})
